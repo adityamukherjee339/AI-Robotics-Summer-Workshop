e7bf9eef16dca8a048d3ad2f6a642c61ab512436
adityamukherjee399 <adityamukherjee072@gmail.com>
2026-06-18 18:15:41 +0530

    Update API URL for Vercel deployment

--- app/components/RegistrationForm.tsx
+++ app/components/RegistrationForm.tsx
@@ -16,7 +16,7 @@ export default function RegistrationForm() {
     setErrorMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/enquiry", {
+      const res = await fetch("/api/enquiry", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
